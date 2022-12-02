731d1be86ee6700a93b9e13d517c6a4cfe389ebc
Пётр Ковригин <piter.kovr@gmail.com>
2022-12-02 20:03:42 +0300

    работа дома по 10 таске

--- js/task10.js
+++ js/task10.js
@@ -1,100 +1,303 @@
-const canvas = document.querySelector('.task10__canvas');
-const ctx = canvas.getContext('2d');
-const figure = new Map([
-	['house', { x: null, y: null, width: 200, height: 100, color: 'rgb(42, 232, 207)', selected: false, }],
-	['window', { x: null, y: null, width: 40, height: 50, color: 'rgb(200, 232, 224)', selected: false, }],
-	['roof', { x: null, y: null, width: 240, height: 50, color: 'rgb(227, 95, 34)', selected: false, }],
-]);
+const canvas = new fabric.Canvas('task10__canvas');
 
-let ClientX = null, ClientY = null, mouseclick = false, isSelected = false;
+let house = new fabric.Rect({ // Форма дома
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(42, 232, 207)',
+	width: 200,
+	height: 100,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let shapeWindow = new fabric.Rect({ // Форма окна + рамка
+	fill: 'rgb(200, 232, 224)',
+	width: 40,
+	height: 50,
+	stroke: 'black',
+});
+let line1 = new fabric.Line([shapeWindow.left + (shapeWindow.width / 2), shapeWindow.top, shapeWindow.left + (shapeWindow.width / 2), shapeWindow.top + shapeWindow.height], { // продольная распорка окна
+	fill: 'black',
+	stroke: 'bkack'
+});
+let line2 = new fabric.Line([shapeWindow.left, shapeWindow.top + shapeWindow.height / 2, shapeWindow.left + shapeWindow.width, shapeWindow.top + shapeWindow.height / 2], { // поперечная распорка окна
+	fill: 'black',
+	stroke: 'bkack'
+});
+let houseWindow = new fabric.Group([shapeWindow, line1, line2], { // окно дома
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let houseRoof = new fabric.Triangle({ // крыша дома
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(227, 95, 34)',
+	width: 240,
+	height: 50,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let housePipe = new fabric.Rect({ // Форма трубы
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(102, 28, 13)',
+	width: 20,
+	height: 40,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let houseDoor = new fabric.Rect({ // Форма трубы
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(92, 85, 84)',
+	width: 40,
+	height: 70,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let foliage1 = new fabric.Triangle({ // верхняя листва
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(56, 199, 40)',
+	width: 150,
+	height: 60,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let foliage2 = new fabric.Triangle({ // средняя листва
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(56, 199, 40)',
+	width: 170,
+	height: 60,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let foliage3 = new fabric.Triangle({ // нижняя листва
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(56, 199, 40)',
+	width: 190,
+	height: 60,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
+let treeTrunk = new fabric.Rect({ // Форма ствола дерева
+	left: Math.floor(Math.random() * 100) + 100,
+	top: Math.floor(Math.random() * 100) + 100,
+	fill: 'rgb(161, 72, 18)',
+	width: 20,
+	height: 280,
+	angle: Math.floor(Math.random() * 180),
+	lockScalingX: true,
+	lockScalingY: true,
+	originX: 'center',
+	originY: 'center',
+});
 
-canvas.addEventListener('mousedown', _ => { mouseclick = true; })
-canvas.addEventListener('mouseup', _ => { mouseclick = false; })
-canvas.addEventListener('mousemove', (e) => { ClientX = e.offsetX; ClientY = e.offsetY; })
+canvas.add(house, houseWindow, houseRoof, housePipe, houseDoor, foliage1, foliage2, foliage3, treeTrunk);
+canvas.sendToBack(treeTrunk);
 
-function drawFigure() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawHouse(figure.get('house'));
-	drawRoof(figure.get('roof'));
-	drawWindow(figure.get('window'));
-
-	if (moveWindow(figure.get('window'))) { }
-	else if (moveRoof(figure.get('roof'))) { }
-	else if (moveHouse(figure.get('house'))) { }
-
-	requestAnimationFrame(drawFigure);
-}
-
-drawFigure();
-
-function drawWindow(value) {
-	initalValues(value);
-	ctx.save();
-	ctx.beginPath();
-	ctx.rect(value.x, value.y, value.width, value.height);
-	ctx.fill();
-	ctx.strokeStyle = 'black';
-	ctx.stroke();
-	ctx.moveTo(value.x + value.width / 2, value.y);
-	ctx.lineTo(value.x + value.width / 2, value.y + value.height);
-	ctx.stroke();
-	ctx.moveTo(value.x, value.y + value.height / 2);
-	ctx.lineTo(value.x + value.width, value.y + value.height / 2);
-	ctx.stroke();
-}
-
-function moveWindow(value) {
-	ctx.beginPath();
-	ctx.rect(value.x, value.y, value.width, value.height);
-	if (mouseclick && ctx.isPointInPath(ClientX, ClientY)) {
-		value.x = ClientX - value.width / 2;
-		value.y = ClientY - value.height / 2;
-		return true;
+function check() {
+	// Выравниваем ствол
+	if (treeTrunk.angle >= 350 || treeTrunk.angle <= 10) {
+		if (treeTrunk.angle != 360 && treeTrunk.angle >= 350) {
+			treeTrunk.animate('angle', 360, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		} else if (treeTrunk.angle != 0 && treeTrunk.angle <= 10) {
+			treeTrunk.animate('angle', 0, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Выравниваем дом
+	if (house.angle >= 350 || house.angle <= 10) {
+		if (house.angle != 360 && house.angle >= 350) {
+			house.animate('angle', 360, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		} else if (house.angle != 0 && house.angle <= 10) {
+			house.animate('angle', 0, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение окна относительно дома
+	if (Math.abs(house.left - 40 - houseWindow.left) < 20 && Math.abs(house.top - 10 - houseWindow.top) < 20 && (Math.abs(house.angle - houseWindow.angle) < 30 || Math.abs(house.angle - houseWindow.angle) > 330)) {
+		if (houseWindow.angle != house.angle) {
+			houseWindow.animate('angle', house.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseWindow.left != house.left - 40) {
+			houseWindow.animate('left', house.left - 40, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseWindow.top != house.top - 10) {
+			houseWindow.animate('top', house.top - 10, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение крыши отностильено дома
+	if (Math.abs(house.left - houseRoof.left) < 20 && Math.abs(house.top - 75 - houseRoof.top) < 20 && (Math.abs(house.angle - houseRoof.angle) < 30 || Math.abs(house.angle - houseRoof.angle) > 330)) {
+		if (houseRoof.left != house.left) {
+			houseRoof.animate('left', house.left, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseRoof.top != house.top - 75) {
+			houseRoof.animate('top', house.top - 75, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseRoof.angle != house.angle) {
+			houseRoof.animate('angle', house.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение двери относительно дома
+	if (Math.abs(house.left + 50 - houseDoor.left) < 20 && Math.abs(house.top + 15 - houseDoor.top) < 20 && (Math.abs(house.angle - houseDoor.angle) < 30 || Math.abs(house.angle - houseDoor.angle) > 330)) {
+		if (houseDoor.left != house.left + 50) {
+			houseDoor.animate('left', house.left + 50, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseDoor.top != house.top + 15) {
+			houseDoor.animate('top', house.top + 15, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (houseDoor.angle != house.angle) {
+			houseDoor.animate('angle', house.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение трубы относительно крыши
+	if (Math.abs(houseRoof.left + 60 - housePipe.left) < 20 && Math.abs(houseRoof.top - 15 - housePipe.top) < 20 && (Math.abs(houseRoof.angle - housePipe.angle) < 30 || Math.abs(houseRoof.angle - housePipe.angle) > 330)) {
+		if (housePipe.left != houseRoof.left + 60) {
+			housePipe.animate('left', houseRoof.left + 60, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (housePipe.top != houseRoof.top - 15) {
+			housePipe.animate('top', houseRoof.top - 15, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (housePipe.angle != houseRoof.angle) {
+			housePipe.animate('angle', houseRoof.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение верхней листвы отностильено ствола
+	if (Math.abs(treeTrunk.left - foliage1.left) < 20 && Math.abs(treeTrunk.top - 140 - foliage1.top) < 20 && (Math.abs(treeTrunk.angle - foliage1.angle) < 30 || Math.abs(treeTrunk.angle - foliage1.angle) > 330)) {
+		if (foliage1.left != treeTrunk.left) {
+			foliage1.animate('left', treeTrunk.left, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage1.top != treeTrunk.top - 140) {
+			foliage1.animate('top', treeTrunk.top - 140, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage1.angle != treeTrunk.angle) {
+			foliage1.animate('angle', treeTrunk.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение средней листвы отностильено ствола
+	if (Math.abs(treeTrunk.left - foliage2.left) < 20 && Math.abs(treeTrunk.top - 80 - foliage2.top) < 20 && (Math.abs(treeTrunk.angle - foliage2.angle) < 30 || Math.abs(treeTrunk.angle - foliage2.angle) > 330)) {
+		if (foliage2.left != treeTrunk.left) {
+			foliage2.animate('left', treeTrunk.left, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage2.top != treeTrunk.top - 80) {
+			foliage2.animate('top', treeTrunk.top - 80, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage2.angle != treeTrunk.angle) {
+			foliage2.animate('angle', treeTrunk.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+	}
+	// Проверяем положение нижней листвы отностильено ствола
+	if (Math.abs(treeTrunk.left - foliage3.left) < 20 && Math.abs(treeTrunk.top - 20 - foliage3.top) < 20 && (Math.abs(treeTrunk.angle - foliage3.angle) < 30 || Math.abs(treeTrunk.angle - foliage3.angle) > 330)) {
+		if (foliage3.left != treeTrunk.left) {
+			foliage3.animate('left', treeTrunk.left, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage3.top != treeTrunk.top - 20) {
+			foliage3.animate('top', treeTrunk.top - 20, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
+		if (foliage3.angle != treeTrunk.angle) {
+			foliage3.animate('angle', treeTrunk.angle, {
+				onChange: canvas.renderAll.bind(canvas),
+				duration: 300,
+			});
+		}
 	}
 }
-
-function drawHouse(value) {
-	initalValues(value);
-	ctx.beginPath();
-	ctx.rect(value.x, value.y, value.width, value.height);
-	ctx.fill();
-}
-
-function moveHouse(value) {
-	ctx.beginPath();
-	ctx.rect(value.x, value.y, value.width, value.height);
-	if (mouseclick && ctx.isPointInPath(ClientX, ClientY)) {
-		value.x = ClientX - value.width / 2;
-		value.y = ClientY - value.height / 2;
-		return true;
-	}
-}
-
-function drawRoof(value) {
-	initalValues(value);
-	ctx.beginPath();
-	ctx.moveTo(value.x, value.y + value.height);
-	ctx.lineTo(value.x + value.width, value.y + value.height);
-	ctx.lineTo(value.x + value.width / 2, value.y);
-	ctx.fill();
-}
-
-function moveRoof(value) {
-	ctx.beginPath();
-	ctx.moveTo(value.x, value.y + value.height);
-	ctx.lineTo(value.x + value.width, value.y + value.height);
-	ctx.lineTo(value.x + value.width / 2, value.y);
-	if (mouseclick && ctx.isPointInPath(ClientX, ClientY)) {
-		value.x = ClientX - value.width / 2;
-		value.y = ClientY - value.height / 2;
-		return true;
-	}
-}
-
-function initalValues(value) {
-	ctx.fillStyle = value.color;
-	if (value.x == null && value.y == null) {
-		value.x = Math.floor(Math.random() * (canvas.width - value.width));
-		value.y = Math.floor(Math.random() * (canvas.height - value.height));
-	}
-}
+setInterval(check, 1000);
